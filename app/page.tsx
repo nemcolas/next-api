1607d5660a5652d2632517ff68e3de7ec0d73a83
Nicolas Martins <nimatins.na1@gmail.com>
2024-04-18 09:45:42 -0300

    feito o metodo FETCH

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import Image from "next/image";
+import {Client}  from "./api/clients/interfaces";
 
-export default function Home() {
-  return (
-    <main>oieeee</main>
-  );
-}
+export default async function Home() {
+  const response = await fetch ('http://localhost:3000/api/clients');
+  const json = await response.json();
+
+return(
+  <main>
+    {
+      json.map((item: Client) =>{
+        return(
+          <>
+          <div className = "border bg-gray-400 justify center">
+          <div className = 'flex 1'>{item.name}</div>
+          <div className = 'uppercase flex 1 italic'>{item.address}</div>
+          </div>
+          </>
+        );
+      })
+    }
+  </main>
+);
+  }
